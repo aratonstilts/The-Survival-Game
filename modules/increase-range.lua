302612732fecd2e5c122aa7da25da72660bb6a12
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2023-02-02 14:11:30 -0500

    Create increase-range.lua

---
+++ modules/increase-range.lua
@@ -0,0 +1,34 @@
+local Players = game:GetService("Players")
+local Player = Players.Player
+
+local function setRangeIncrease(value)
+    Player:SetAttribute("increaseRange", value)
+end
+
+local function pickUpItem(part)
+    game:GetService("ReplicatedStorage").remoteInterface.inventory.pickupItem:FireServer(part)
+end
+
+local function getDrops()
+    local dropsFolder = game:GetService("Workspace").droppedItems
+    for i,v in pairs(dropsFolder:GetChildren()) do
+        pickUpItem(v)
+    end
+end
+
+local function increasePickupRange()
+    setRangeIncrease(true)
+    
+    while Player:GetAttribute("increaseRange") and wait(0.2) do
+        getDrops()
+    end
+end
+
+local function regularPickupRange()
+    setRangeIncrease(false)
+end
+
+return {
+    increasePickupRange = increasePickupRange, 
+    regularPickupRange = regularPickupRange
+}
